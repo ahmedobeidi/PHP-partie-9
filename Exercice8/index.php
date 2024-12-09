6313924bd96097c59ec2e81795ecef646ab1252d
ahmedobeidi <alobeidi.dev@gmail.com>
2024-12-09 09:45:14 +0100

    DONE: Exercise 8

---
+++ Exercice8/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 7</title>
+</head>
+<body>
+    <?php 
+        $dateNow = new DateTime();
+        $dateNow = $dateNow->getTimestamp();
+
+        $diff = 22 * 24 * 60 * 60;
+        
+        $dateBefor22Days = $dateNow - $diff;
+
+        echo date("d/m/Y", $dateBefor22Days);
+    ?>
+</body>
+</html>
